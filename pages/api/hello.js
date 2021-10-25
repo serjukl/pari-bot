4428ae2d5871f494f28ba9335dc8fbf0a80dd6d2
serjukl <serjukl98@gmail.com>
2021-10-25 22:33:56 +0300

    first commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const request = require('request');
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  const URL = 'https://www.ferra.ru/ru/techlife/news/';
+  request(URL, function (err, res, body) {
+      if (err) throw err;
+      console.log(JSON.stringify({body}));
+      res.status(200).json(body)
+  });
 }
